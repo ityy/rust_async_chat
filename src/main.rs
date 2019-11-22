ed4777e81f55134bbc0ee5d7ef6af3f87efb2a1f
yangyang <ofyang@qq.com>
2019-11-22 18:17:20 +0800

    完成t02

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,21 @@
+//! 规格与说明
+//! 聊天使用基于TCP的简单文本协议。该协议包含utf-8消息，以\n分隔。
+//! 客户端连接到服务器，并作为第一行发送登录信息。之后，客户端可以使用以下语法将消息发送给其他客户端：
+//! login1, login2, ... loginN: message
+//! 然后，每个指定的客户端都会收到一条 from login: message 消息。
+//! 可能的会话如下所示:
+//! On Alice's computer:   |   On Bob's computer:
+//!
+//! > alice                |   > bob
+//! > bob: hello               < from alice: hello
+//!                        |   > alice, bob: hi!
+//!                            < from bob: hi!
+//! < from bob: hi!        |
+//!
+//! 聊天服务器的主要挑战是跟踪许多并发连接。聊天客户端的主要挑战是管理并发传出消息，传入消息和用户键入。
+
+
 mod t01_accept_loop;
 mod t02_receiving_messages;
+
+
--- src/t02_receiving_messages.rs
+++ src/t02_receiving_messages.rs
@@ -1,11 +1,13 @@
-use std::io::{BufRead, BufReader};
-
 use async_std::{
     net::{TcpListener, ToSocketAddrs}, // 3
     prelude::*, // 1
     task, // 2
 };
-use futures::{AsyncReadExt, FutureExt, StreamExt};
+use async_std::{
+    io::BufReader,
+    net::TcpStream,
+};
+use futures::{AsyncReadExt, FutureExt, StreamExt, Future};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
@@ -19,24 +21,30 @@ async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     while let Some(stream) = incoming.next().await {
         let stream = stream?;
         println!("Accepting from:{}", stream.peer_addr()?);
-        let _handle = task::spwan(cannection_loop(stream));
+        //我们使用task :: spawn函数来生成一个独立的任务以与每个客户端一起工作。也就是说，接受客户端后，accept_loop立即开始等待下一个客户端。跟java接收到客户端后新建线程处理类似, 只不过这里使用的是异步任务.
+        //这是事件驱动的体系结构的核心优势：我们同时为许多客户端提供服务，而无需花费很多硬件线程。
+        let _handle = task::spawn(connection_loop(stream));
     }
 
     Ok(())
 }
 
 async fn connection_loop(stream: TcpStream) -> Result<()> {
+    //幸运的是，“将字节流分成几行”功能已经实现。 .lines（）调用返回String的流。
     let reader = BufReader::new(&stream);
     let mut lines = reader.lines();
 
+    //处理首行-->登陆信息
     let name = match lines.next().await {
         None => Err("peer disconnected immediately")?,
         Some(line) => line?,
     };
     println!("name={}", name);
 
+    //而且，我们再次实现了手动异步for循环。
     while let Some(line) = lines.next().await {
         let line = line?;
+        //最后，我们将每一行解析为目标登录名和消息本身的列表。
         let (dest, msg) = match line.find(':') {
             None => continue,
             Some(idx) => (&line[..idx], line[idx + 1..].trim()),
@@ -52,4 +60,28 @@ async fn connection_loop(stream: TcpStream) -> Result<()> {
 fn run() -> Result<()> {
     let fut = accept_loop("127.0.0.1:8080");
     task::block_on(fut) //
+}
+
+/// 处理错误
+/// 上述解决方案中的一个严重问题是，尽管我们在connection_loop中正确传播了错误，但随后将错误放到了 floor 上！
+/// 也就是说，task :: spawn不会立即返回错误（它不能，它需要先运行Future才能完成），只有在它加入后才返回。
+/// 我们可以通过等待任务加入来“修复”它，如下所示：
+/// ```
+/// let handle = task::spawn(connection_loop(stream));
+/// handle.await
+/// ```
+/// .await等待客户端完成，然后？传播结果。
+/// 但是，此解决方案有两个问题！首先，因为我们立即等待客户端，所以我们一次只能处理一个客户端，这完全违反了异步的目的！
+/// 其次，如果客户端遇到IO错误，则整个服务器将立即退出。也就是说，一个人的不稳定连接使整个聊天室瘫痪了！
+/// 在这种情况下，处理客户端错误的正确方法是记录它们，并继续为其他客户端提供服务。
+/// 因此，我们为此使用一个辅助函数：
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+    where
+        F: Future<Output=Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln!("{}", e)
+        }
+    })
 }
